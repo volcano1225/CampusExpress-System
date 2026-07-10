881978ca7c912356cf05ea64a575a543d6dbad08
canyang Li <244735855+volcano1225@users.noreply.github.com>
2026-07-10 19:27:54 +0800

    Add files via upload

---
+++ Express.BLL/AdminInfoBLL.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Express.Model;
+using Express.DAL;
+
+namespace Express.BLL
+{
+    public class AdminInfoBLL
+    {
+        AdminInfoDAL dal = new AdminInfoDAL();
+        public DataTable GetList(string keyword) { return dal.GetAll(keyword); }
+        public bool Add(AdminInfo a) { return dal.Add(a); }
+        public bool Update(AdminInfo a) { return dal.Update(a); }
+        public bool Delete(int id) { return dal.Delete(id); }
+        public bool ResetPassword(int id, string pwd) { return dal.ResetPwd(id, pwd); }
+        public DataTable Login(string user, string pwd) { return dal.GetByLogin(user, pwd); }
+    }
+}
